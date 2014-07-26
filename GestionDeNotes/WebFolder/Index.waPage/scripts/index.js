57f10b42055342b38b0ee81ef76842a286ab3cd6
Jeddi Fouad <jeddi.fouad@gmail.com>
2014-07-26 05:02:42 +0100

    Update #8

--- GestionDeNotes/WebFolder/Index.waPage/scripts/index.js
+++ GestionDeNotes/WebFolder/Index.waPage/scripts/index.js
@@ -22,17 +22,17 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	button9.click = function button9_click (event)// @startlock
 	{// @endlock
-		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+		waf.sources.Etudiants3.filterQuery("Nom=:1",'qString');
 	};// @lock
 
 	button8.click = function button8_click (event)// @startlock
 	{// @endlock
-		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+		waf.sources.Etudiants2.filterQuery("Nom=:1",'qString');
 	};// @lock
 
 	button7.click = function button7_click (event)// @startlock
 	{// @endlock
-		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+		waf.sources.Etudiants1.filterQuery("Nom=:1",'qString');
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
