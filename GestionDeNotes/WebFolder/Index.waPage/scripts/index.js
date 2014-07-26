50de8e1c1a8d7dec4eacfd5865ce9991fbdf99da
Jeddi Fouad <jeddi.fouad@gmail.com>
2014-07-26 04:51:45 +0100

    Update #7

--- GestionDeNotes/WebFolder/Index.waPage/scripts/index.js
+++ GestionDeNotes/WebFolder/Index.waPage/scripts/index.js
@@ -2,11 +2,11 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button2 = {};	// @button
+	var button10 = {};	// @button
+	var button9 = {};	// @button
+	var button8 = {};	// @button
+	var button7 = {};	// @button
 	var button1 = {};	// @button
-	var button4 = {};	// @button
-	var button5 = {};	// @button
-	var button6 = {};	// @button
 	var menuItem5 = {};	// @menuItem
 	var menuItem4 = {};	// @menuItem
 	var menuItem3 = {};	// @menuItem
@@ -15,30 +15,29 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // eventHandlers// @lock
 
-	button2.click = function button2_click (event)// @startlock
+	button10.click = function button10_click (event)// @startlock
 	{// @endlock
-		
-		waf.sources.Etudiants1.filterQuery("Nom=:1",'qString'+'*');
+		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+	};// @lock
+
+	button9.click = function button9_click (event)// @startlock
+	{// @endlock
+		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+	};// @lock
+
+	button8.click = function button8_click (event)// @startlock
+	{// @endlock
+		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
+	};// @lock
+
+	button7.click = function button7_click (event)// @startlock
+	{// @endlock
+		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
-		waf.sources.Etudiants.filterQuery("Nom=:1",'qString'+'*');
-	};// @lock
-
-	button4.click = function button4_click (event)// @startlock
-	{// @endlock
-		waf.sources.Etudiants2.filterQuery("Nom=:1",'qString'+"*");
-	};// @lock
-
-	button5.click = function button5_click (event)// @startlock
-	{// @endlock
-		waf.sources.Etudiants3.filterQuery("Nom=:1",'qString'+"*");
-	};// @lock
-
-	button6.click = function button6_click (event)// @startlock
-	{// @endlock
-		waf.sources.Etudiants4.filterQuery("Nom=:1",'qString'+"*");
+		waf.sources.Etudiants.filterQuery("Nom=:1",'qString');
 	};// @lock
 
 	menuItem5.click = function menuItem5_click (event)// @startlock
@@ -62,11 +61,11 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button10", "click", button10.click, "WAF");
+	WAF.addListener("button9", "click", button9.click, "WAF");
+	WAF.addListener("button8", "click", button8.click, "WAF");
+	WAF.addListener("button7", "click", button7.click, "WAF");
 	WAF.addListener("button1", "click", button1.click, "WAF");
-	WAF.addListener("button4", "click", button4.click, "WAF");
-	WAF.addListener("button5", "click", button5.click, "WAF");
-	WAF.addListener("button6", "click", button6.click, "WAF");
 	WAF.addListener("menuItem5", "click", menuItem5.click, "WAF");
 	WAF.addListener("menuItem4", "click", menuItem4.click, "WAF");
 	WAF.addListener("menuItem3", "click", menuItem3.click, "WAF");
